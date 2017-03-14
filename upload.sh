a1407d69f39b0a3dc629629a2424adc09fae7ecb
xu-hao <xuh@email.unc.edu>
2017-03-14 16:10:05 -0400

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,6 +1,11 @@
-for i in `seq 1 2700`; do
+for i in `seq 1 2648`; do
   for j in `seq 1 1000`; do
     iput afilea $i.$j
     echo $i.$j
   done
 done
+
+ for j in `seq 1 671`; do
+    iput afilea 2649.$j
+    echo 2649.$j
+  done
